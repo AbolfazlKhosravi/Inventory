acc61f145f4d0e81b760e1e02ec55f63ec271ab9
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2023-02-14 01:38:22 +0330

    fix bug

--- src/js/storage.js
+++ src/js/storage.js
@@ -22,9 +22,9 @@ export default class Storage {
   static getAllProducts(sort="latest") {
     const products = JSON.parse(localStorage.getItem("products")) || [];
     return products.sort((a, b) => {
-      if(sort==="latest"){
+      if(sort=="latest"){
         return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
-      }else if(sort==="earliest"){
+      }else{
         return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1;
       }
     });
